da52412ecea4f48f3ebc2c644f0226edd93acbff
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-05-29 23:32:00 +0600

    dalily ui fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-red-700'>
-      test
+    <div  >
+      <h1 className='text-red-600'>arpon</h1>
+      <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
     </div>
   )
 }
